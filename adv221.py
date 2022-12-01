dc94d34b1026e60b8011e7253895e328163b8017
bert2tag <robert.logan@tag-games.com>
2022-12-01 15:18:11 +0000

    advent day 1 part 1

---
+++ adv221.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import sys
+import pprint
+
+def max_elf(elfs):
+    maxcals = 0
+    maxelf = 0
+    for elf in elfs:
+        if elfs[elf] > maxcals:
+            maxcals = elfs[elf]
+            maxelf = elf
+        
+    return maxelf,maxcals
+
+def has_num(s):
+    return any(i.isdigit() for i in s)
+
+pp = pprint.PrettyPrinter(indent=4)
+
+elfs = {}
+elfname = 1
+for line in sys.stdin:
+    if has_num(line):
+        if elfname in elfs:
+            elfs[elfname] += int(line)
+        else:
+            elfs[elfname] = int(line)
+    else:
+        elfname += 1
+        
+#        print("xx:{}".format(line))
+#        print("en:{}".format(elfname))
+#        print("ee:{}".format(elfname))
+#pp.pprint(elfs)
+maxelf,maxcals = max_elf(elfs)
+print("elf {}, cals: {}".format(maxelf,maxcals))
