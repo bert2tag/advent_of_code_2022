6e0e800141ee258a25593e993c58aad1bc741c94
bert2tag <robert.logan@tag-games.com>
2022-12-02 17:02:40 +0000

    part2 of 2

--- adv222.py
+++ adv222.py
@@ -2,39 +2,46 @@
 import sys
 
 def value(a):
-    if a == 'Rock': return 1
-    if a == 'Paper': return 2
-    if a == 'Scissors': return 3
+    if (a == 'Rock'): return 1
+    if (a == 'Paper'): return 2
+    if (a == 'Scissors'): return 3
 
 def nicename(a):
     if (a == 'A') or (a == 'X'): return 'Rock'
     if (a == 'B') or (a == 'Y'): return 'Paper'
     if (a == 'C') or (a == 'Z'): return 'Scissors'
+    return a
 
-def win(a,b): 
-
+def win(a): 
     if a == 'Rock':
-        return b == 'Scissors'
+        return 'Scissors'
     if a == 'Paper':
-        return b == 'Rock'
+        return 'Rock'
     if a == 'Scissors':
-        return b == 'Paper'
+        return 'Paper'
+
+def lose(a):
+    if a == 'Rock':
+        return 'Paper'
+    if a == 'Paper':
+        return 'Scissors'
+    if a == 'Scissors':
+        return 'Rock'
 
 def totals(a,b):
     a = nicename(a)
-    b = nicename(b)
-
-    if a == b:
+    if b == 'Y': # draw
         return 3 + value(a)
-
-    if win(a,b):
-        return 6 + value(a)
-    else:
+    if b == 'X': 
+        a = win(a)
         return 0 + value(a)
+    if b == 'Z': 
+        a = lose(a)
+        return 6 + value(a)
 
 score = 0
 for line in sys.stdin:
-    b,a = line.split()
-    print("{} {}: {}".format(nicename(a),nicename(b),totals(a,b)))
+    a,b = line.split()
+    #print("{} {}".format(nicename(a),b))
     score += totals(a,b)
 print("score: {}".format(score))
